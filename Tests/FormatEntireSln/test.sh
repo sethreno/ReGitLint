7c891adf62c5f044220a8d6c354314e51ec5f6b7
Seth Reno <sethreno@gmail.com>
2022-11-24 01:11:28 +0000

    Fix permissions issue with test

--- Tests/FormatEntireSln/test.sh
+++ Tests/FormatEntireSln/test.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 dotnet tool restore
+
+# give everyone write permissions
+# without doing this I recieved the following error from jb
+#
+#   error: Files still read-only: /srv/Tests/HelloWorld/Program.cs
+sudo chmod -R 777 HelloWorld
+
 ../ReGitLint/bin/Release/$1/ReGitLint
 
 diff HelloWorld/Program.cs FormatEntireSln/Expected/Program.cs
